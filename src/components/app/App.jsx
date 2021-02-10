2053a1daef48711657bb4560c160d51b6b2561d1
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-09 22:19:56 -0800

    scaffolding

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Header from '../header/Header';
+import AllCharacters from '../../containers/allCharacters/AllCharacters';
+import Details from '../../containers/details/Details';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route 
+            path="/" 
+            exact
+            component={AllCharacters} 
+          />
+          <Route 
+            path="/:id" 
+            exact
+            component={Details} 
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
---
+++ src/containers/allCharacters/AllCharacters.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AllCharacters() {
+  return (
+    <div>
+      <h1>Placeholder for allCharacters</h1>
+    </div>
+  );
+}
+
+export default AllCharacters;
