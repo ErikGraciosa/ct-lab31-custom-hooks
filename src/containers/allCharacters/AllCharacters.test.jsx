7e922e0bc6eb8b17a3df19c79a6e7a729ef07f2d
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-10 07:52:57 -0800

    added the mock server

--- src/containers/allCharacters/AllCharacters.test.jsx
+++ src/containers/allCharacters/AllCharacters.test.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import AllCharacters from './AllCharacters';
+import mockApiData from '../../fixtures/mockApiData.json';
+
+const server = setupServer(
+  rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
+    return res(ctx.json(mockApiData));
+  })
+);
 
 describe('AllCharacters container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
   it('fetches and displays all characters to home page', () => {
     act(() => {
       render(<AllCharacters />);
--- src/containers/details/Details.test.jsx
+++ src/containers/details/Details.test.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import Details from './Details';
+import mockApiData from '../../fixtures/mockApiData.json';
+
+const server = setupServer(
+  rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
+    return res(ctx.json(mockApiData));
+  })
+);
 
 describe('One character details container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+  
   it('fetches and displays the details for one character', () => {
     render(<Details match={{ params: { id: '1' } }}/>);
     
